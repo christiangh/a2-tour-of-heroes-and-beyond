c99a748467701ce4bcbdc96499869aa09be6f3fe
Christian García <siuke1@hotmail.com>
2016-10-02 19:17:13 +0100

    Add HeroService. Add data in mock-heroes. Move css in AppComponent to app.component.css.

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,8 @@ import { FormsModule }   from '@angular/forms'
 import { AppComponent }	from './app.component';
 
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+import { HeroService } from './shared/services/hero.service';
 	
 
 @NgModule({
